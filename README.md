76cc63e37cfdffefb76cfdb8a4b313fc17c42686
Rohit Shah <shahrohit2061@gmail.com>
2024-06-06 22:35:58 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HTML EMAIL BUILDER
+This is the drag and drop html email builder
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-3xl text-center font-bold">HTML Drag and Drop Email Builder</h1>
+<div>heelo</div>
